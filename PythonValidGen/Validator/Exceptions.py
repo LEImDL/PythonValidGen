820a23262fc9dafbfb0c515c4d4a1ad07a4d9728
LEImDL <86199648+LEImDL@users.noreply.github.com>
2021-06-27 00:07:37 +0100

    Add files via upload

--- PythonValidGen/Validator/Exceptions.py
+++ PythonValidGen/Validator/Exceptions.py
@@ -21,21 +21,21 @@ class ExceptionValidation(Exception):
             202 -> Exceeds Minimum length\n
             300 -> Multiples values for singleton value\n
             400 -> Not Encoded in Base64\n
-            401 -> String value isn't String\n
+            401 -> String value is not String\n
             402 -> Not Encoded in given Encoding\n
             500 -> Value not defined in Enum\n
-            600 -> Numerical value isn't Integer\n
-            601 -> Numerical value isn't Numeric\n
+            600 -> Numerical value is not Integer\n
+            601 -> Numerical value is not Numeric\n
             700 -> Invalid length (numeric representation)\n
             701 -> Exceeds Maximum value\n
             702 -> Exceeds Minimum value\n
-            800 -> Boolean value isn't Boolean\n
-            900 -> Date value isn't String\n
+            800 -> Boolean value is not Boolean\n
+            900 -> Date value is not String\n
             901 -> Incorrect format for date\n
             902 -> Future date, but past requested\n
             903 -> Past date, but future requested\n
-            904 -> Exceeds maximum years limit\n
-            905 -> Exceeds minimum years limit
+            904 -> Exceeds Maximum years limit\n
+            905 -> Exceeds Minimum years limit
 
         args: *List
             The placeholders' values used for `format` function
