0218c189b1eed3cc6efcd81fcca39ce5148100a7
LEImDL <86199648+LEImDL@users.noreply.github.com>
2021-07-09 18:43:07 +0100

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='PythonValidGen',
-    version='1.5.5',
+    version='1.6.0',
     author='Ana Almeida, André Figueiredo, Luís Ferreira',
     packages=find_packages(),
     package_data={
